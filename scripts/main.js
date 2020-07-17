db1ec09b593ae6b3cdf2466948ede48912bb8062
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-17 10:22:01 -0500

    Added list of Manufacturing companies

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import {allBusinesses} from "./BusinessList.js"
 import {logBusinesses} from "./BusinessProvider.js"
 import { newYorkBussinesses} from "./NewYorkCompanies.js"
+import {manufacturingBussinesses} from "./Manufacturing.js"
 
 allBusinesses()
 logBusinesses()
 newYorkBussinesses()
+manufacturingBussinesses()
